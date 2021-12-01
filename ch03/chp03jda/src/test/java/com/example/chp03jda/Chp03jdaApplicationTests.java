4da86be46616b07d471a0841da5b241bf9a4c63a
FabioPita18 <fabiopita267@gmail.com>
2021-12-01 09:33:49 +0200

    2021/12/01 (1)

---
+++ ch03/chp03jda/src/test/java/com/example/chp03jda/Chp03jdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chp03jda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chp03jdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
